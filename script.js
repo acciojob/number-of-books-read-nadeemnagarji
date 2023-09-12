7592866ccd21577cd85033cc8722111baf4d62f4
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-12 13:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,14 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+	 let ans = 0;
+ for (let i = 0 ;i<library.length ; i++){
+	if(library[i].readingStatus == true ){
+		ans++;
+	}
+	 
+ }
+	return ans;
 };
 
 // Do not change the code below
